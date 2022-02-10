72d7d23b5e3107928286e938af7ece3e85163beb
Ken Austria <kenaustriadev@gmail.com>
2022-02-10 20:56:00 +0900

    MongoDB Setup

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
-const express = require("express")
-const app = express()
-const mongoose = require("mongoose")
+const express = require('express');
+const app = express();
+const mongoose = require('mongoose');
+const dotenv = require("dotenv")
 
-app.listen(9000, () => {
-	console.log("sanity check")
-})
+dotenv.config()
+
+mongoose.connect(process.env.MONDODB_URL)
+	.then(() => console.log("MongoDB Connection Successful"))
+	.catch((error) => console.log(error))
+
+app.listen( process.env.PORT || 9000, () => {
+  console.log('sanity check');
+});
