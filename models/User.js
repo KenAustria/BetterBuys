765485e3a115fa07b0a1b69cdc60052daec7ae8f
Ken Austria <kenaustriadev@gmail.com>
2022-02-18 14:39:00 +0900

    product schema

--- models/User.js
+++ models/User.js
@@ -1,5 +1,4 @@
 const mongoose = require("mongoose");
-const { boolean } = require("webidl-conversions");
 
 const UserSchema = new mongoose.Schema(
 	{
@@ -7,9 +6,11 @@ const UserSchema = new mongoose.Schema(
 		email: { type: String, required: true, unique: true },
 		password: { type: String, required: true },
 		isAdmin: {
-			type: boolean,
+			type: Boolean,
 			default: false
 		}
 	},
 	{ timestamp: true }
 )
+
+module.exports = mongoose.model("User", UserSchema)
