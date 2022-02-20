764c78b835b2fd07fd263f74a48422d3c0445f1e
Ken Austria <kenaustriadev@gmail.com>
2022-02-20 14:07:39 +0900

    jsonwebtoken

--- routes/auth.js
+++ routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 // SIGNUP
 router.post('/signup', async (req, res) => {
@@ -34,6 +35,16 @@ router.post('/signin', async (req, res) => {
     );
     const OriginalPassword = hashedPassword.toString(CryptoJS.enc.Utf8);
 
+    // to verify if cart and order belongs to user
+    const accessToken = jwt.sign(
+      {
+        id: user.id,
+        isAdmin: user.isAdmin,
+      },
+      process.env.JWT_SECRET,
+      { expiresIn: '5d' }
+    );
+
     // if password does not match
     OriginalPassword !== req.body.password &&
       res.status(401).json('Wrong credentials!');
@@ -43,7 +54,7 @@ router.post('/signin', async (req, res) => {
     const { password, ...others } = user._doc;
 
     // others = reveal all of user's info except password
-    res.status(200).json(others);
+    res.status(200).json({ ...others, accessToken });
   } catch (error) {
     res.status(500).json(error);
   }
