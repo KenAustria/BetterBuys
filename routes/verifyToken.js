781f2faed997265a6b3dbc3b8427e2bb66ce9b13
Ken Austria <kenaustriadev@gmail.com>
2022-02-20 15:02:08 +0900

    verifyTokenAndAuthorization fn

---
+++ routes/verifyToken.js
@@ -0,0 +1,27 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+  const authHeader = req.headers.token;
+  if (authHeader) {
+    jwt.verify(token, process.env.JWT_SECRET, (error, user) => {
+      if (error) res.status(403).json('Token is not valid!!');
+      req.user = user;
+      next();
+    });
+  } else {
+    return res.status(401).json('You are not authenticated!');
+  }
+};
+
+const verifyTokenAndAuthorization = (req, res, next) => {
+  verifyToken(req, res, () => {
+    // if password input matches password stored OR is an admin
+    if (req.user.id === req.params.id || req.user.isAdmin) {
+      next();
+    } else {
+      res.status(403).json('You are not allowed!');
+    }
+  });
+};
+
+modify.exports = { verifyToken, verifyTokenAndAuthorization };
