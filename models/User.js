e5dabc3ac65def8ea5c155c42db00598bd8b12f2
Ken Austria <kenaustriadev@gmail.com>
2022-02-20 14:10:11 +0900

    files updated with prettier format

--- models/User.js
+++ models/User.js
@@ -1,17 +1,17 @@
-const mongoose = require("mongoose");
-const { boolean } = require("webidl-conversions");
+const mongoose = require('mongoose');
+const { boolean } = require('webidl-conversions');
 
 const UserSchema = new mongoose.Schema(
-	{
-		username: { type: String, required: true, unique: true },
-		email: { type: String, required: true, unique: true },
-		password: { type: String, required: true },
-		isAdmin: {
-			type: Boolean,
-			default: false
-		}
-	},
-	{ timestamp: true }
-)
+  {
+    username: { type: String, required: true, unique: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    isAdmin: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  { timestamp: true }
+);
 
-module.exports = mongoose.model("User", UserSchema)
+module.exports = mongoose.model('User', UserSchema);
