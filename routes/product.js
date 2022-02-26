8ea9ea70ddc51ae93012af15752dac1380b8bd00
Ken Austria <kenaustriadev@gmail.com>
2022-02-26 17:08:28 +0900

    get products route

--- routes/product.js
+++ routes/product.js
@@ -31,20 +31,45 @@ router.put('/:id', verifyTokenAndAdmin, async (req, res) => {
 });
 
 // DELETE PRODUCT
-router.delete("/:id", verifyTokenAndAdmin, async (req, res) => {
+router.delete('/:id', verifyTokenAndAdmin, async (req, res) => {
   try {
     await Product.findByIdAndDelete(req.params.id);
-    res.status(200).json("Product has been deleted!");
+    res.status(200).json('Product has been deleted!');
   } catch (error) {
     res.status(500).json(error);
   }
 });
 
 // GET PRODUCT
-router.get("/find/:id", async (req, res) => {
+router.get('/find/:id', async (req, res) => {
   try {
     const product = await Product.findById(req.params.id);
     res.status(200).json(product);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// GET PRODUCTS
+router.get('/', async (req, res) => {
+  const queryNew = req.query.new;
+  const queryCategory = req.query.category;
+  try {
+    let products;
+
+    if (queryNew) {
+      products = await Product.find().sort({ createdAt: -1 }).limit(1);
+    } else if (queryCategory) {
+      products = await Product.find({
+        categories: {
+          $in: [queryCategory],
+        },
+      });
+    } else {
+      products = await Product.find();
+    }
+
+    res.status(200).json(products);
   } catch (error) {
     res.status(500).json(error);
   }
