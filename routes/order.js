3bb7e63d10813d1c4d6dc0b15b8a3830e701fa76
Ken Austria <kenaustriadev@gmail.com>
2022-03-08 15:19:14 +0900

    create order route

--- routes/order.js
+++ routes/order.js
@@ -1,3 +1,19 @@
+const Order = require('../models/Order');
 const router = require('express').Router();
+const {
+  verifyToken
+} = require('./verifyToken');
+
+// CREATE ORDER
+router.post('/', verifyToken, async (req, res) => {
+  const newOrder = new Order(req.body);
+
+  try {
+    const savedOrder = await newOrder.save();
+    res.status(200).json(savedOrder);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
