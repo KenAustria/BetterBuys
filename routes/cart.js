bc59ac10a6af29d27b9570e361dbacb8c343ef1e
Ken Austria <kenaustriadev@gmail.com>
2022-03-08 14:42:57 +0900

    create cart route

--- routes/cart.js
+++ routes/cart.js
@@ -1,3 +1,17 @@
+const Cart = require('../models/Cart');
 const router = require('express').Router();
+const { verifyToken } = require('./verifyToken');
+
+// CREATE CART
+router.post('/', verifyToken, async (req, res) => {
+  const newCart = new Cart(req.body);
+
+  try {
+    const savedCart = await newCart.save();
+    res.status(200).json(savedCart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
 
 module.exports = router;
