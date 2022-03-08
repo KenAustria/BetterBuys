41e3770a19a2bac04987c4c51c9d6f3e5c996709
Ken Austria <kenaustriadev@gmail.com>
2022-03-08 14:46:53 +0900

    update cart route

--- routes/cart.js
+++ routes/cart.js
@@ -1,6 +1,6 @@
 const Cart = require('../models/Cart');
 const router = require('express').Router();
-const { verifyToken } = require('./verifyToken');
+const { verifyToken, verifyTokenAndAuthorization } = require('./verifyToken');
 
 // CREATE CART
 router.post('/', verifyToken, async (req, res) => {
@@ -9,6 +9,22 @@ router.post('/', verifyToken, async (req, res) => {
   try {
     const savedCart = await newCart.save();
     res.status(200).json(savedCart);
+  } catch (error) {
+    res.status(500).json(error);
+  }
+});
+
+// UPDATE CART
+router.put('/:id', verifyTokenAndAuthorization, async (req, res) => {
+  try {
+    const updatedCart = await Cart.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    res.status(200).json(updatedCart);
   } catch (error) {
     res.status(500).json(error);
   }
