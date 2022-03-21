bb5aaa84281fb80e37568d5ffcaa2ccf2f048c0b
Ken Austria <kenaustriadev@gmail.com>
2022-03-21 13:08:17 +0900

    Navbar and Promotion component with styles

---
+++ client/better_buys/src/pages/Home/Home.js
@@ -0,0 +1,13 @@
+import Promotion from '../../components/Promotion/Promotion';
+import Navbar from '../../components/Navbar/Navbar';
+
+const Home = () => {
+  return (
+    <div>
+      <Promotion />
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
