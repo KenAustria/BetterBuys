77b2fa1284dd3e58e0d55eae2ba25aafb7dba2dd
Ken Austria <kenaustriadev@gmail.com>
2022-03-21 17:34:10 +0900

    carousel component and styles

---
+++ client/src/data.js
@@ -0,0 +1,23 @@
+export const carouselSlides = [
+  {
+    id: 1,
+    img: 'https://ibb.co/XtcJLCX',
+    title: 'PRODUCTS YOU LOVE, PROTECTED',
+    desc: 'SHOW YOUR NEW DEVICE SOME LOVE AND KEEP IT PROTECTED CARE+',
+    bg: 'd9d2e9',
+  },
+  {
+    id: 2,
+    img: 'https://ibb.co/q72HYLh',
+    title: 'LIGHT. BRIGHT. FULL OF MIGHT',
+    desc: 'YOUR PRODUCT GOES EVERYWHERE WITH YOU',
+    bg: 'cfe2f3',
+  },
+  {
+    id: 3,
+    img: 'https://ibb.co/JRpYGy4',
+    title: 'SHOP SAFELY AND CONFIDENTLY',
+    desc: 'SHOP WITH AN EXPERT TO KNOW OUR SAFETY PROCEDURES',
+    bg: 'ead1dc',
+  },
+];
--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,11 +1,13 @@
 import Promotion from '../../components/Promotion/Promotion';
 import Navbar from '../../components/Navbar/Navbar';
+import Carousel from '../../components/Carousel/Carousel';
 
 const Home = () => {
   return (
     <div>
       <Promotion />
       <Navbar />
+      <Carousel />
     </div>
   );
 };
