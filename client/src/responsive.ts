93483baefc43223e469081142d7caeccadc9ce1f
Ken Austria <kenaustriadev@gmail.com>
2023-02-12 19:52:02 +0900

    added new breakpoints

--- client/src/responsive.ts
+++ client/src/responsive.ts
@@ -1,25 +1,65 @@
 import { css } from 'styled-components';
 
-export const mobile = props => {
-	return css`
-    @media only screen and (max-width: 360px) {
-      ${props}
-    }
-  `;
+export const extraSmallDevicePortrait = (props) => {
+    return css`
+        @media (min-width: 576px) {
+            ${props}
+        }
+    `;
 };
 
-export const tablet = props => {
-	return css`
-    @media only screen and (max-width: 768px) {
-      ${props}
-    }
-  `;
+export const smallDeviceLandscape = (props) => {
+    return css`
+        @media (min-width: 576px) {
+            ${props}
+        }
+    `;
 };
 
-export const desktop = props => {
-	return css`
-    @media only screen and (max-width: 1366px) {
-      ${props}
-    }
-  `;
+export const mediumDevice = (props) => {
+    return css`
+        @media (min-width: 768px) {
+            ${props}
+        }
+    `;
+};
+
+export const largeDevice = (props) => {
+    return css`
+        @media (min-width: 992px) {
+            ${props}
+        }
+    `;
+};
+
+export const extraLargeDevice = (props) => {
+    return css`
+        @media (min-width: 1200px) {
+            ${props}
+        }
+    `;
+};
+
+export const mobile = (props) => {
+    return css`
+        @media only screen and (max-width: 360px) {
+            ${props}
+        }
+    `;
+};
+
+export const tablet = (props) => {
+    return css`
+        @media only screen and (max-width: 768px) {
+            ${props}
+        }
+    `;
+};
+
+export const desktop = (props) => {
+    return css`
+        @media only screen and (max-width: 1366px) {
+            ${props}
+        }
+    `;
 };
